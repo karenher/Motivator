606dcf5374dbe4cb00d8a72114bc0a06c6b7534a
Karen Her <karenher@seas.upenn.edu>
2017-03-31 18:33:54 -0400

    Adding basic setup of project

---
+++ MotivateApp/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './button_style.css';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div className="button">
+        <span>Click Here</span>
+      </div>
+    );
+  }
+}
+
+export default Button;
---
+++ MotivateApp/webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var config = {
+  entry: './main.js',
+
+  output: {
+    path: path.resolve(__dirname, './'),
+    filename: 'index.js'
+  },
+
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  }
+}
+
+module.exports = config;
